d9219e99974f65d638e5f8f7f758716fced9a82e
jose-emanuel-gabriel-ocenar <ocenarjeggy@gmail.com>
2026-02-20 22:30:17 +0800

    update

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,10 +1,34 @@
-
 const app = Vue.createApp({
   data() {
     return {
       name: "Jeggy Ocenar",
       role: "Aspiring System Administrator",
-      description: "Interested in system architecture, servers, and clean infrastructure design."
+
+      about: "I am passionate about system infrastructure, server management, and building scalable architectures. I enjoy learning how systems work behind the scenes.",
+
+      skills: [
+        "Linux Basics",
+        "Networking Fundamentals",
+        "HTML & CSS",
+        "JavaScript",
+        "Vue.js"
+      ],
+
+      goal: "To become a skilled System Administrator and eventually move into System Architecture.",
+
+      available: true
+    }
+  },
+
+  computed: {
+    statusButton() {
+      return this.available ? "Set as Busy" : "Set as Available"
+    }
+  },
+
+  methods: {
+    toggleStatus() {
+      this.available = !this.available
     }
   }
 })
