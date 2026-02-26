6f2a7d18d378f70d16abb714138869d340db2054
jose-emanuel-gabriel-ocenar <ocenarjeggy@gmail.com>
2026-02-26 22:53:52 +0800

    api

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,15 +1,13 @@
-
 import { createApp } from 'vue'
-
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
+import RestApi from './components/RestApi.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
-app.component('food-item2', FoodItem2)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
-app.mount('#app')  
+app.component('rest-api', RestApi)
+app.mount('#app')
