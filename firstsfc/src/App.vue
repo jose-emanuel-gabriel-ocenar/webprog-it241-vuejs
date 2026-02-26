e3565cd2c6bf17915ecc11ab0b84006c67b2ea0d
jose-emanuel-gabriel-ocenar <ocenarjeggy@gmail.com>
2026-02-26 22:50:14 +0800

    mali haha

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,13 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from 'src/lib/supabaseClient.js'
+
+const instruments = ref([])
+
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+
+onMounted(() => {
+   getInstruments()
+})
+</script>
+
 <template>
-  <h1>Food</h1>
-
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <comment-form/>
-  <comment/>
-  <rest-api/> 
-
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
 </template>
-
-<script></script>
